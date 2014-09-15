961c466abea4b750eaafcf31a7b50bd173653afa
Giacomo Vacca <giacomo.vacca@gmail.com>
2014-09-15 12:05:38 +0100

    Created a base node definition, from which personal nodes can inherit. Enabled TCP to test latest changes.

--- environments/test/manifests/nodes/asterisk_nodes.pp
+++ environments/test/manifests/nodes/asterisk_nodes.pp
@@ -1,7 +1,14 @@
 #nodes.pp
 
-node 'YOURHOSTNAMEHERE' {
+node 'base_asterisk' {
 
   class { '::asterisk':
+    service_manage => true,
+    service_enable => true,
+    service_ensure => 'running',
+    tcpenable => 'yes',
   }
+}
+
+node 'debian7' inherits 'base_asterisk' {
 }
