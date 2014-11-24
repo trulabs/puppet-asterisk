a9f20a416ef3e69e4ae7263ce704116e8443c66b
Chad Walstrom <chad.walstrom@govdelivery.com>
2014-11-24 15:23:49 -0600

    Extend the user.pp to include more options
    Added was the `read`, `write`, `permit`, `deny`, and `eventfilter`
    options. Moved the content to an ERB template.

--- manifests/config/user.pp
+++ manifests/config/user.pp
@@ -16,6 +16,41 @@
 #  [*secret*]
 #    Secret to log in with. Default to empty string ("").
 #
+#  [*read*]
+#    String VALUE
+#    Default: undef
+#
+#    If defined, place the string VALUE as a keypair "read = VALUE"
+#    for the user.
+#
+#  [*write*]
+#    String VALUE
+#    Default: undef
+#
+#    If defined, place the string VALUE as a keypair "write = VALUE"
+#    for the user.
+#
+#  [*permit*]
+#    Array of Strings
+#    Default: undef
+#
+#    If defined, place the multiple lines of keypairs "permit = VALUE"
+#    for the user.
+#
+#  [*deny*]
+#    Array of Strings
+#    Default: undef
+#
+#    If defined, place the multiple lines of keypairs "deny = VALUE"
+#    for the user.
+#
+#  [*eventfilter*]
+#    Array of Strings
+#    Default: undef
+#
+#    If defined, place the multiple lines of keypairs "eventfilter = VALUE"
+#    for the user.
+#
 # === Example
 # Add a user 'asterisk' with the secret 'asterisk' for AMI service
 # configuration.
@@ -27,20 +62,33 @@
 #     # Add an AMI user
 #     asterisk::config::user{'/etc/asterisk/manager.conf':
 #         user   => 'ami_user',
-#         secret => 'ami_secret',}
+#         secret => 'ami_secret',
+#         read   => 'all',
+#     }
 #     # Add an ARI user
 #     asterisk::config::user{'/etc/asterisk/ari.conf':
 #         user   => hiera('asterisk::ari::user','ari_user),
 #         secret => hiera('asterisk::ari::secret','ari_secret'),}
 #
-define asterisk::config::user ($target = $title, $user, $secret="") {
+define asterisk::config::user (
+  $target      = $title,
+  $user,
+  $secret      = "",
+  $read        = "",
+  $write       = "",
+  $deny        = [],
+  $permit      = [],
+  $eventfilter = [],
+) {
   validate_string($target, $user, $secret)
+  validate_string($read, $write)
+  validate_array($permit, $deny, $eventfilter)
 
   # Ensure unique title based on target and username
   $safe_title = regsubst("${target}_user_${user}",'[/\.]','_')
   concat::fragment{$safe_title:
     target  => $target,
-    content => "[${user}]\nsecret = ${secret}\n",
+    content => template('asterisk/user.erb'),
     order   => 20
   }
 }
--- tests/manager.pp
+++ tests/manager.pp
@@ -1,15 +1,21 @@
 # Test the manager user configuration
 node default {
   class { '::asterisk':
-    service_manage  => true,
-    service_ensure  => 'running',
-    tcpenable       => 'yes',
-    manage_config   => true,
-    manager_enabled => 'yes',
+    service_manage     => true,
+    service_ensure     => 'running',
+    tcpenable          => 'yes',
+    manage_config      => true,
+    manager_enabled    => 'yes',
+    manager_webenabled => 'yes',
   }
   # Add an AMI user
   ::asterisk::config::user{'/etc/asterisk/manager.conf':
-    user    => 'manager_user',
-    secret  => 'manager_secret',
+    user        => 'manager_user',
+    secret      => 'manager_secret',
+    read        => 'all',
+    write       => 'all',
+    permit      => ['127.0.0.1/255.255.255.255'],
+    deny        => ['192.168.1.0/255.255.255.0','172.16.0.0/255.240.0.0'],
+    eventfilter => ['!Event: RTCP*','!Variable: RTPAUDIOQOS*'],
   }
 }
